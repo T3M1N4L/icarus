8136af915c4dbbfdc904bd4ae6ff321dddec9fb9
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2025-03-15 16:39:34 +0000

    astro rewrite

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import "/src/styles/global.css";`,
+        },
+      },
+    },
+  },
+});
--- httpmitm/dmbackend/start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "$@"
-SCRIPTDIR=$(readlink -f "$(dirname "$0")")
-echo "Script directory: $SCRIPTDIR"
-pushd "${SCRIPTDIR}" || exit
-echo "now in scriptdir"
-echo "${SCRIPTDIR}/server.py"
-python3 "$SCRIPTDIR"/server.py 
